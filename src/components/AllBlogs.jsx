fce31f38e3ed877cc154701221e54b9d6f2499bb
shaheen2013 <shaheenmediusware@gmail.com>
2024-06-26 19:30:13 +0600

    API integration on category list.

--- src/components/AllBlogs.jsx
+++ src/components/AllBlogs.jsx
@@ -1,6 +1,21 @@
+import { useEffect, useState } from "react";
 import BlogCard from "./BlogCard";
 
 const AllBlogs = () => {
+  const [categories, setCategories] = useState([]);
+  const [activeCategory, setActiveCategory] = useState("case-studies");
+  const getBlogCategories = async () => {
+    const res = await fetch(
+      "https://hr.mediusware.xyz/api/website/blogs/categories/"
+    );
+    const data = await res.json();
+    setCategories(data);
+  };
+
+  useEffect(() => {
+    getBlogCategories();
+  }, []);
+
   return (
     <div className="container">
       <div className="flex items-center justify-between gap-3 md:flex-row flex-col">
@@ -9,42 +24,20 @@ const AllBlogs = () => {
         </p>
       </div>
       <div className="flex items-center flex-wrap justify-center gap-4 sm:py-12 py-5">
-        <div>
-          <button
-            className={` sm:py-[11px] py-1  sm:px-6 px-4 border rounded-3xl text-white bg-[#0060AF]  `}
-          >
-            All{" "}
-            <span
-              className={`px-[6px] py-1  rounded-lg ms-1  text-[#008F79] bg-[#EAECF0] h-[20px] `}
+        {categories?.map((item, index) => (
+          <div key={index}>
+            <button
+              className={` sm:py-[11px] py-1  sm:px-6 px-4 border rounded-xl ${item.slug === activeCategory ? "bg-[#0060AF] text-white" : "bg-white" } flex items-center justify-center`}
             >
-              10
-            </span>
-          </button>
-        </div>
-        <div>
-          <button
-            className={` sm:py-[11px] py-1  sm:px-6 px-4 border rounded-3xl bg-white`}
-          >
-            Frontend{" "}
-            <span
-              className={`px-[6px] py-1  rounded-lg ms-1 bg-[#EAECF0] h-[20px] `}
-            >
-              5
-            </span>
-          </button>
-        </div>
-        <div>
-          <button
-            className={` sm:py-[11px] py-1  sm:px-6 px-4 border rounded-3xl bg-white`}
-          >
-            Node.js{" "}
-            <span
-              className={`px-[6px] py-1  rounded-lg ms-1 bg-[#EAECF0] h-[20px]`}
-            >
-              5
-            </span>
-          </button>
-        </div>
+              {item.name}{" "}
+              <span
+                className={`rounded-full ms-1  text-slate-700 bg-[#EAECF0] h-[20px] w-[20px] flex items-center justify-center`}
+              >
+                {item.total_blog}
+              </span>
+            </button>
+          </div>
+        ))}
       </div>
       <div className="grid grid-cols-3 pb-4 gap-y-12">
         <BlogCard />
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    cors: false,
+    proxy: {
+      "/api": {
+        target: "https://hr.mediusware.xyz/api/website/blogs/categories",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+  optimizeDeps: {
+    // include: ['linked-dep'],
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+      // include: [/linked-dep/, /node_modules/],
+    },
+  },
   plugins: [react()],
-})
+});
