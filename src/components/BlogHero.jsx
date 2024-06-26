313cc5d11c152ae8fb378e57057534551785d8cf
shaheen <coredeveloper.2013@gmail.com>
2024-06-26 16:26:30 +0600

    remove search box

--- src/components/BlogHero.jsx
+++ src/components/BlogHero.jsx
@@ -10,39 +10,6 @@ const BlogHero = () => {
               READ INSIGHTFUL <span className="text-[#00A88E]">ARTICLES</span>
             </h2>
           </div>
-
-          <div className=" flex items-center justify-center lg:mt-12 md:mt-8 mt-6">
-            <div className="flex items-center max-w-[544px] w-full shadow rounded-lg">
-              <div className="relative flex-1">
-                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                  <span className="text-gray-500">
-                    <svg
-                      className="w-5 h-5"
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M21 21l-6-6m4-2a8.5 8.5 0 11-17 0 8.5 8.5 0 0117 0z"
-                      ></path>
-                    </svg>
-                  </span>
-                </div>
-                <input
-                  type="Search"
-                  id="Search"
-                  name="search"
-                  className="bg-white py-2 px-4 ps-10 border-0 border-gray-300 rounded-l-lg focus:outline-none focus:border-blue-500 flex-1 sm:h-[56px] h-[48px]  w-full"
-                  placeholder="Search"
-                />
-              </div>
-              <Button>Search</Button>
-            </div>
-          </div>
         </div>
       </div>
     </>
